8855c912f720669391726c294c420da2b15f224c
Shubh <shubhisroking@proton.me>
2024-08-08 17:19:00 +0530

    add todo stuff
    Signed-off-by: Shubh <shubhisroking@proton.me>

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'comment', 'vimdoc' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
