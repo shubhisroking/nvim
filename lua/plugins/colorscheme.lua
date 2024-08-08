ccf517115cb2d7b5c7e854627a03003969b6a6ca
Shubh <shubhisroking@proton.me>
2024-08-08 21:09:40 +0530

    fix colorscheme
    Signed-off-by: Shubh <shubhisroking@proton.me>

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,39 @@
 return {
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
+    'catppuccin/nvim',
+    name = 'catppuccin',
     priority = 1000,
     config = function()
-      vim.cmd([[colorscheme catppuccin]])
+      require('catppuccin').setup {
+        no_italic = true,
+        term_colors = true,
+        styles = {
+          comments = {},
+          conditionals = {},
+          loops = {},
+          functions = {},
+          keywords = {},
+          strings = {},
+          variables = {},
+          numbers = {},
+          booleans = {},
+          properties = {},
+          types = {},
+        },
+        color_overrides = {
+          mocha = {
+            base = '#000000',
+            mantle = '#000000',
+            crust = '#000000',
+          },
+        },
+        integrations = {
+          telescope = {
+            enabled = true,
+          },
+        },
+      }
+      vim.cmd [[colorscheme catppuccin]]
     end,
   },
 }
